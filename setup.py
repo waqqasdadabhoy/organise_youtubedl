8e20e45bc168104fdea1999e1b1d5d2f83a7b161
Waqqas Dadabhoy <waqqas.dadabhoy@gmail.com>
2015-07-15 18:36:11 +0500

    Create setup.py for pip package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='organise_youtubedl',
+    version='0.0.1',
+    packages=['organise_youtubedl'],
+    url='',
+    license='',
+    author='Waqqas Dadabhoy',
+    author_email='waqqas.dadabhoy@gmail.com',
+    description='Organise files downloaded using youtube-dl, and create a list with descriptions',
+    install_requires=[
+        'youtube_dl',
+    ],
+)
