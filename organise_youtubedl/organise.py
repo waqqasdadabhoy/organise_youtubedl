5c15ed0c5d0707d9f752c5c85e79eabdacd81057
Waqqas Dadabhoy <waqqas.dadabhoy@gmail.com>
2015-07-15 20:24:37 +0500

    Replace call to youtube-dl executable with import. Shorten variable names.

--- organise_youtubedl/organise.py
+++ organise_youtubedl/organise.py
@@ -2,28 +2,34 @@ import os
 import shutil
 import subprocess
 import json
+import youtube_dl
 
 
-def create_dir(uploader: str, config):
-    if not os.path.exists(config['organised_files_dir'] + "/" + uploader):
-        os.makedirs(config['organised_files_dir'] + "/" + uploader)
+
+def create_dir(uploader: str, conf):
+    if not os.path.exists(conf['organised_files_dir'] + "/" + uploader):
+        os.makedirs(conf['organised_files_dir'] + "/" + uploader)
 
 
-def check_if_file_is_youtube_download(filename: str):
+def is_file_youtube_download(filename: str):
     if filename[-16:-15] == "-":
         if not (" " in filename[-15:-4]):
             return True
     return False
 
-def organise(config):
+def organise(conf):
+    ydl = youtube_dl.YoutubeDL(conf['ydl_opts'])
+
     for file in os.listdir("."):
         if file.endswith(".mp4") or file.endswith(".flv"):
-            if check_if_file_is_youtube_download(file):
+            if is_file_youtube_download(file):
                 try:
-                    json_data = json.loads(subprocess.check_output(["youtube-dl","-j", "https://www.youtube.com/watch?v="+file[-15:-4]]).decode("utf-8"))
-                except subprocess.CalledProcessError:
+                    result = ydl.extract_info("https://www.youtube.com/watch?v="+file[-15:-4], download=False)
+                    uploader_id = result['uploader_id']
+                    #json_data = json.loads(subprocess.check_output(["youtube-dl","-j", "https://www.youtube.com/watch?v="+file[-15:-4]]).decode("utf-8"))
+                except youtube_dl.utils.DownloadError:
                     continue
-                print(json_data["uploader_id"], "/", file)
+                print(uploader_id, "/", file)
 
-                create_dir(json_data["uploader_id"])
-                shutil.move(file, config['organised_files_dir'] + "/" + json_data["uploader_id"])
+                create_dir(uploader_id)
+                shutil.move(file, conf['organised_files_dir'] + "/" + uploader_id)
